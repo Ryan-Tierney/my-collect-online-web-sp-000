7c446b00b15cc52531fc2160197c04c351c4c60e
Ryan William Tierney <ryan.tierney92@gmail.com>
2020-01-25 04:29:22 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -10,9 +10,11 @@ my_collect(["Ryan", "Vanessa", "Gatlyn", "Aryan"])
 do |name| 
   name.upcase  
  end 
+ end 
 
   
 my_collect(["Ryan Tierney", "Vanessa Tierney"])
 do |name|
   name.split(" ").first
+ end 
  end 
